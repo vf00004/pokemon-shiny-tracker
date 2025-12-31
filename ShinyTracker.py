7b8592514a804fc141df432a9adce341cbc23e1b
vf00004 <vf00004@my.westga.edu>
2025-12-31 11:49:22 -0500

    Add Pokemon input validation
    - Restructure code for json compatibility

--- ShinyTracker.py
+++ ShinyTracker.py
@@ -1,31 +1,52 @@
 import keyboard
 import os
-import requests
 import json
 
-poke_api_url = 'https://pokeapi.co/api/v2/pokemon-species?limit=100000'
+POKEMON_NAMES = 'pokemon_species.json'
+database = None
+tracked_mon = None
 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-clear_console()
-print('Welcome to the Pokemon Shiny Tracker!')
-tracked_mon = input('Enter the name of the Pokemon to shiny hunt: ')
-encounters = 0
+def open_species_json():
+    global database 
+    with open(POKEMON_NAMES, 'r', encoding='utf-8') as f:
+        database = json.load(f)
+        
+def check_mon_validity(tracked_mon):
+    global database
+    if tracked_mon.lower() in database:
+        return True
 
-while True:
+def get_tracked_mon():
+    global tracked_mon
+    is_valid = False
+    while not is_valid:
+        tracked_mon = input('Enter the name of the Pokemon to shiny hunt: ')
+        is_valid = check_mon_validity(tracked_mon)
+
+if __name__ == '__main__':
+    open_species_json()
     clear_console()
-    print('Hunting a shiny', tracked_mon, '...')
-    print('Controls: ↑ = increment, ↓ = decrement, ESC = quit program')
-    print('Encounters:', encounters)
-    key_event = keyboard.read_event()
     
-    if key_event.event_type == keyboard.KEY_DOWN:
-        if key_event.name == 'up':
-            encounters+=1
-        elif key_event.name == 'down':
-            if encounters - 1 >= 0:
-                encounters-=1
-        elif key_event.name == 'esc':
-            print('Goodbye!')
-            break
+    print('Welcome to the Pokemon Shiny Tracker!')
+    get_tracked_mon()
+    encounters = 0
+
+    while True:
+        clear_console()
+        print('Hunting a shiny', tracked_mon, '...')
+        print('Controls: ↑ = increment, ↓ = decrement, ESC = quit program')
+        print('Encounters:', encounters)
+        key_event = keyboard.read_event()
+        
+        if key_event.event_type == keyboard.KEY_DOWN:
+            if key_event.name == 'up':
+                encounters += 1
+            elif key_event.name == 'down':
+                if encounters - 1 >= 0:
+                    encounters -= 1
+            elif key_event.name == 'esc':
+                print('Goodbye!')
+                break
