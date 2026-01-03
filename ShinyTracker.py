7464e7c353556cc8c3d130c887c879c2fe87a3c5
vf00004 <vf00004@my.westga.edu>
2026-01-03 18:54:21 -0500

    Add function to save user data to JSON
    - Persist tracked user data to 'shiny_progress.json'
    - Remove unnecessary global variables to increase modularity

--- ShinyTracker.py
+++ ShinyTracker.py
@@ -4,7 +4,7 @@ import json
 
 POKEMON_NAMES = 'pokemon_species.json'
 database = None
-tracked_mon = None
+tracked_data = {}
 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -20,24 +20,36 @@ def check_mon_validity(tracked_mon):
         return True
 
 def get_tracked_mon():
-    global tracked_mon
     is_valid = False
     while not is_valid:
         tracked_mon = input('Enter the name of the Pokemon to shiny hunt: ')
         is_valid = check_mon_validity(tracked_mon)
+    return tracked_mon
+        
+def update_progress(tracked_mon, encounters_so_far):
+    global tracked_data
+    tracked_data = {"pokemon": tracked_mon,
+                    "encounters": encounters_so_far}
+
+def save_progress():
+    with open('shiny_progress', 'w', encoding="utf-8") as f:
+        json.dump(tracked_data, f, indent=2)
+
 
 if __name__ == '__main__':
     open_species_json()
     clear_console()
     
     print('Welcome to the Pokemon Shiny Tracker!')
-    get_tracked_mon()
+    tracked_mon = get_tracked_mon()
     encounters = 0
-
+    update_progress(tracked_mon, encounters)
+    save_progress()
+    
     while True:
         clear_console()
         print('Hunting a shiny', tracked_mon, '...')
-        print('Controls: ↑ = increment, ↓ = decrement, ESC = quit program')
+        print('Controls: ↑ = increment, ↓ = decrement, ESC = save & quit program')
         print('Encounters:', encounters)
         key_event = keyboard.read_event()
         
@@ -48,5 +60,7 @@ if __name__ == '__main__':
                 if encounters - 1 >= 0:
                     encounters -= 1
             elif key_event.name == 'esc':
-                print('Goodbye!')
+                update_progress(tracked_mon, encounters)
+                save_progress()
+                print('Progress saved! Goodbye.')
                 break
